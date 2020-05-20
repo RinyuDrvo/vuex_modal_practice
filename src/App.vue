aa956956180957fab58586b6038df01584f695a4
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-20 23:35:23 +0000

    make main view

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <button @click="basicModalShow">basicModalShow</button>
+    <basic-modal></basic-modal>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapMutations } from 'vuex';
+import BasicModal from './components/BasicModal.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'app',
+  components: {
+    BasicModal
+  },
+  methods: {
+    ...mapMutations('basicModal', {
+      basicModalShow: 'show'
+    })
   }
 }
+</script>
+
+<style lang="scss">
+@import "./assets/style";
 </style>
