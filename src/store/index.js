052ab94118cebe8fd46a515bff95af86377615a5
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-20 23:35:59 +0000

    make  store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import basicModal from './modules/basicModal'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+const state = {
+  //
+};
+
+const modules = {
+  basicModal
+};
+
+const store = new Vuex.Store({
+  state,
+  modules
+});
+
+export default store;
